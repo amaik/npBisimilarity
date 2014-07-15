c45009ed5520ed21daf2a319c97bf4a307872008
Alexander Speicher <alex.speicher@gmx.net>
2014-07-15 14:01:33 +0200

    j

--- src/com/pseuco/project/Main.java
+++ src/com/pseuco/project/Main.java
@@ -313,8 +313,8 @@ public class Main {
 
 		MinimizationMonitor moni = new MinimizationMonitor(lts.getStates(),
 				lts.getWeakTransitionRelation());
-		//int avProc = Runtime.getRuntime().availableProcessors();
-		int avProc = 1;
+		int avProc = Runtime.getRuntime().availableProcessors();
+		//int avProc = 1;
 		Thread[] threads = new Thread[avProc + 1];
 		for (int i = 0; i <= avProc; i++) {
 			Thread freddi = new Thread(moni.runner);
